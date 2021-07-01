e66b355331a89aad0167728811db802071afacc8
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-07-01 18:45:18 +0600

    Adding laravel ui

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'shakil ahmed',
+            'email' => 'shakil@gmail.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
